c79887edcb229c8a4727e26d082f6710695cff55
Adrián <amartinv1105@gmail.com>
2021-06-12 14:26:25 +0200

    Now the owner is displayed in the marketplace list and downloaded schools list

--- resources/views/schools/marketplace.blade.php
+++ resources/views/schools/marketplace.blade.php
@@ -10,6 +10,7 @@
                     <th>{{__('Title')}}</th>
                     <th>{{__('Description')}}</th>
                     <th>{{__('Created')}}</th>
+                    <th>{{__('Owner')}}</th>
                     <th class="actions">{{__('Actions')}}</th>
                 </tr>
             </thead>
@@ -24,6 +25,9 @@
                     </td>
                     <td>
                         {{$school->created_at}}
+                    </td>
+                    <td>
+                        {{$school->user->email}}
                     </td>
                     <td class="actions">
                         <x-form.action-button color="green" type="post" class="fa-download" :href="route('schools.getSchool', ['school'=>$school])" />
