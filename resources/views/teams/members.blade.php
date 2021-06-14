23359ef07d76ffdf367dfdb9ee0b11adc943740f
Adrián <amartinv1105@gmail.com>
2021-06-14 13:58:45 +0200

    Added check icon if member is joined

--- resources/views/teams/members.blade.php
+++ resources/views/teams/members.blade.php
@@ -23,8 +23,11 @@
                     <td>
                         {{$user->email}}
                     </td>
-                    <td>
-                        {{$user->pivot->joined}}
+                    <td>@if($user->pivot->joined == true)
+                        <i class="rounded-full p-2 my-1 bg-green-500 text-white transition-colors duration-200 transform hover:bg-green-800 fas fa-check red"></i>
+                        @else
+                        <i class="rounded-full p-2 my-1 bg-red-500 text-white transition-colors duration-200 transform hover:bg-red-800 fas fa-check red"></i>
+                        @endif
                     </td>
                     <td class="actions">
                     </td>
