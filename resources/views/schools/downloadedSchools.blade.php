bbb073bc4c864856e6fbc5f67edfc13c96a5363f
wertons <32491515+wertons@users.noreply.github.com>
2021-06-14 19:32:39 +0200

    Added front ent filtering

--- resources/views/schools/downloadedSchools.blade.php
+++ resources/views/schools/downloadedSchools.blade.php
@@ -15,18 +15,30 @@
                 </tr>
             </thead>
             <tbody>
-                @foreach ($schools as $i=>$school)
                 <tr>
                     <td>
+                        <input type="text" class="marketplace-searcher" name="title"> </td>
+                    <td>
+                        <input type="text" class="marketplace-searcher" name="description"> </td>
+                    <td>
+                        <input type="text" class="marketplace-searcher" name="created"> </td>
+                    <td>
+                        <input type="text" class="marketplace-searcher" name="owner"> </td>
+                    <td class="actions">
+                    </td>
+                </tr>
+                @foreach ($schools as $i=>$school)
+                <tr>
+                    <td class="searchable-title searchable">
                         {{$school->title}}
                     </td>
-                    <td>
+                    <td class="searchable-description searchable">
                         {{$school->description}}
                     </td>
-                    <td>
+                    <td class="searchable-created searchable">
                         {{$school->created_at}}
                     </td>
-                    <td>
+                    <td class="searchable-email searchable">
                         {{$school->user->email}}
                     </td>
                     <td class="actions">
@@ -40,4 +52,23 @@
 
         </table>
     </div>
+    @push('scripts')
+    <script>
+        document.querySelectorAll(".marketplace-searcher").forEach(input => {
+            input.addEventListener("change", (ev) => {
+            
+                let text = ev.target.value; 
+                document.querySelectorAll(".searchable-" + ev.target.name).forEach(row => {
+                    if (!row.innerHTML.includes(text.trim())) {
+                        row.parentElement.classList.add("hidden");
+                    } else{
+                        row.parentElement.classList.remove("hidden");
+
+                    }
+                });
+            });
+        });
+
+    </script>
+    @endpush
 </x-app-layout>
